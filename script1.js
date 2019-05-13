89db29e48ffd679fa3e13851b82e9dba5e90e171
Dayanand <48797265+DayaPadala1@users.noreply.github.com>
2019-05-14 00:59:53 +0200

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,11 @@
+(function (window) {
+  var Janana = {};
+  JananaGreeter.name = "John";
+  var greeting = "goodbye ";
+  johnGreeter.saygoodbye = function () {
+    console.log(greeting + johnGreeter.name);
+  }
+
+  window.johnGreeter = johnGreeter;
+
+})(window);
